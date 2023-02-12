ffcf98c755f5368cb28d5a4d884d2cd9e5c0633e
DESKTOP-OHD6DA9\USER <zufall2277@gmail.com>
2023-02-13 03:50:00 +0900

    no message

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -7,7 +7,11 @@ public class Spawner : MonoBehaviour
 {
     private GameObject prefab;
     private GameManager gameManager;
+    private Vector3 mousePos;
+    private Vector3 transPos;
+    private Vector3 targetPos;
     public float prefabHeight;
+    public Camera mainCamera;
 
     void Start()
     {
@@ -20,10 +24,19 @@ public class Spawner : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)) 
+        if(Input.GetMouseButton(0))
+        {
+            mousePos = Input.mousePosition;
+            transPos = Camera.main.ScreenToWorldPoint(mousePos);
+            targetPos = new Vector3(transPos.x, transform.position.y, transPos.y);
+            this.gameObject.transform.position = targetPos;
+        }
+
+        if(Input.GetMouseButtonUp(0)) 
         {   
             Instantiate(prefab,this.transform.position, Quaternion.identity);
             transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y + prefabHeight, transform.position.z), 0.001f);
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y + prefabHeight, mainCamera.transform.position.z), 0.001f);
             prefab = gameManager.burgers[Random.Range(1, gameManager.burgers.Length)];
         }
     }
